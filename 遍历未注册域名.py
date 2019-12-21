f8dc704eb2286eaf75cf99c80ff1c4050c373efa
1436057869 <imust@mail.bccto.me>
2019-12-21 10:53:08 +0800

    register-domain

---
+++ 遍历未注册域名.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#-*-coding:utf8-*-
+# Author JaesonCheng
+# Date 2017-05-16
+ 
+import urllib.request
+from multiprocessing import Pool
+import itertools as its
+
+def writelog(mfile,message):
+    with open(mfile,'a+') as f:
+        f.write(message)
+ 
+def createdomain(ws,num,sfx):
+    r = its.product(ws,repeat=num)
+    for i in r:
+        domain = ''.join(i) + sfx
+        yield domain
+
+def checkdomainstatus(domain):
+    API = "http://panda.www.net.cn/cgi-bin/check.cgi?area_domain="
+    url = API + domain
+    try:
+        xhtml = urllib.request.urlopen(url,timeout=5).read()
+    except:
+        return
+    r1 = xhtml.find(b'<original>211')      # 纯数字表示 已经被注册
+    r2 = xhtml.find(b'<original>210')      # 纯数字表示 还未被注册
+    # r1 = xhtml.find(r'<original>211')      # 字符串表示 已经被注册
+    # r2 = xhtml.find(r'<original>210')      # 字符串表示 还未被注册
+    if r2 != -1:
+        # print domain
+        writelog('domain_enable.txt',domain+'\n')
+
+if __name__ == "__main__":
+    words = '1234567890'
+    len = 5
+    suffix = '.cn'
+    domains = createdomain(words,len,suffix)
+    task_pool = Pool(4)
+    results = task_pool.map(checkdomainstatus,domains)
+    task_pool.close()
+    task_pool.join()
